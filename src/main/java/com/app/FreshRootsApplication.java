ff0fab2a31d4bc39d97ace967a2195c3378b9397
Badrinath Pathak <85957166+badri00@users.noreply.github.com>
2024-08-23 23:11:40 +0530

    Add files via upload

---
+++ src/main/java/com/app/FreshRootsApplication.java
@@ -0,0 +1,33 @@
+package com.app;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class FreshRootsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshRootsApplication.class, args);
+	}
+	
+	// add a method to configure ModelMapper bean as a spring bean (to be managed by SC)
+	@Bean
+	public ModelMapper mapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
+	}
+	
+	//configure password encode bean required for authentication manager
+//		@Bean
+//		public PasswordEncoder encoder()
+//		{
+//			return new BCryptPasswordEncoder();
+//		}
+
+}
